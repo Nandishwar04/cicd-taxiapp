d5cae0a4080599e802dc81d9d2878b3ba2995b34
Nandishwar04 <nandishwar22112@gmail.com>
2025-12-07 20:28:34 +0530

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Kubectl apply -f deployement.yaml
-Kubectl apply -f service.yaml
-Kubectl apply -f secrets.yaml
-Kubectl apply -f namespace.yaml
+kubectl apply -f deployement.yaml
+kubectl apply -f service.yaml
+kubectl apply -f secrets.yaml
+kubectl apply -f namespace.yaml
